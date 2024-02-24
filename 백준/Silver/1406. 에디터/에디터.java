182e19230aba6adb6a362c14d1f99b758aec9e61
jeongjihun <83577481+MasJeong@users.noreply.github.com>
2024-02-24 17:09:55 +0900

    [Silver II] Title: 에디터, Time: 668 ms, Memory: 144644 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+
+/**
+ * 에디터 - 자료구조 TODO 다시 풀기
+ * 1. stack을 이용하여 push 시 pointer + 1에 값이 있으면 뒤로 1칸씩 옮긴다.
+ * - pop 시 pointer부터 size -1까지 앞으로 옮긴다.
+ * -> 시간초과.
+ */
+public class Main {
+    private static final LinkedList<Character> linkedList= new LinkedList<>();
+
+    private static int idx = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String n = br.readLine();
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        // 처음 입력받은 문자열 push
+        for (int i = 0; i < n.length(); i++) {
+            linkedList.add(n.charAt(i));
+        }
+
+        idx = linkedList.size();
+
+        ListIterator<Character> listIterator = linkedList.listIterator();
+
+        while (listIterator.hasNext()) {
+            listIterator.next();
+        }
+
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            char cmd = st.nextToken().charAt(0);
+
+            switch (cmd) {
+                case 'L':
+                    if (listIterator.hasPrevious()) {
+                        listIterator.previous();
+                    }
+                    break;
+                case 'D':
+                    if (listIterator.hasNext()) {
+                        listIterator.next();
+                    }
+                    break;
+                case 'B':
+                    if (listIterator.hasPrevious()) {
+                        listIterator.previous();
+                        listIterator.remove();
+                    }
+                    break;
+                case 'P':
+                    listIterator.add(st.nextToken().charAt(0));
+                    break;
+            }
+        }
+
+        linkedList.forEach(sb::append);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
