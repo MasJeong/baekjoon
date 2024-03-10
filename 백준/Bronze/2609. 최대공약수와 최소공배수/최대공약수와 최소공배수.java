2b6564c339dd015f1bbd5d9f4bed1b38f0710c04
jeongjihun <83577481+MasJeong@users.noreply.github.com>
2024-03-10 14:44:23 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,26 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * 최대공약수와 최소공배수
+ */
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int maxIdx = 0;
-		sc.close();
-		int max = 0;
-		
-		if (num1 > num2)
-			max = num1;
-		else
-			max = num2;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 1; i <= max; i++) {
-			if ((num1 % i == 0) && (num2 % i == 0))
-				maxIdx = i;
-		}
-		System.out.println(maxIdx);
-		System.out.println(num1 * num2 / maxIdx);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-	}
+        // 최대공약수
+        int max = 0;
+
+        int a = Math.max(n, m);
+
+        for (int i = 1; i <= a; i++) {
+            if (n % i == 0 && m % i == 0) {
+                max = i;
+            }
+        }
+
+        // 최대공약수
+        sb.append(max).append("\n");
+        // 최소공배수 = n * m / 최대공약수
+        sb.append(n * m / max);
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
 }
