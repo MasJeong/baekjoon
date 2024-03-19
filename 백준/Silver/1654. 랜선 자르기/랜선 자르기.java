19ff7dd9551a99af30aad82fe904ed18b3f5f961
jeongjihun <83577481+MasJeong@users.noreply.github.com>
2024-03-19 21:09:31 +0900

    [Silver II] Title: 랜선 자르기, Time: 196 ms, Memory: 17112 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 랜선 자르기 - 이분 탐색
+ */
+public class Main {
+
+    private static long binarySearch(int[] arr, int n) {
+        long left = 0;
+        // 가장 큰 수를 기준으로 이분탐색 진행
+        long right = arr[arr.length - 1];
+        long result = 1;
+
+        while (left <= right && (left + right > 1)) {
+            // int의 경우 left와 right 둘 다 2^31-1인 경우 오버플로우가 발생한다.
+            long mid = (left + right) >>> 1;
+            int cntLine = 0;
+
+            // mid가 0이 되는 경우가 존재한다.
+            for (int i : arr) cntLine += (int) (i / mid);
+
+            if (cntLine < n) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+                result = mid;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[k];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = binarySearch(arr, n);
+
+        System.out.println(answer);
+        br.close();
+    }
+
+}
