fb9f8af94aaa22c84de6176411637e1ddf4c6bf0
jeongjihun <83577481+MasJeong@users.noreply.github.com>
2024-04-17 21:02:04 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14336 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,20 +8,19 @@ public class Main {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final StringBuilder sb = new StringBuilder();
         final int NUMBER = 31;
+        final int M = 1234567891;
 
-        int l = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-
-        // 오버플로우 체크 필요
         long sum = 0L;
+        long pow = 1L;
 
-        for (int i = 0; i < str.length(); i++) {
-            long a = str.charAt(i) - 96;
-
-            sum += a * ((long) Math.pow(NUMBER, i));
+        for (int i = 0; i < L; i++) {
+            sum += (str.charAt(i) - 96) * pow;
+            pow = (pow * NUMBER) % M;
         }
 
-        sb.append(sum);
+        sb.append(sum % M);
 
         System.out.println(sb);
         br.close();
