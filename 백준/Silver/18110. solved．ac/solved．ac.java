46012e01fa5f7d6606ae60df605b90b2a826474c
jeongjihun <83577481+MasJeong@users.noreply.github.com>
2024-04-19 15:31:54 +0900

    [Silver IV] Title: solved.ac, Time: 380 ms, Memory: 37896 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arrDifficulty = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arrDifficulty[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arrDifficulty);
+
+        int avgCount = Math.round(n * ((float) 15 / 100));
+        int sum = 0;
+
+        for (int i = avgCount; i < arrDifficulty.length - avgCount; i++) {
+            sum += arrDifficulty[i];
+        }
+
+        int result = Math.round((float) sum / (arrDifficulty.length - avgCount - avgCount));
+
+        System.out.println(result);
+        br.close();
+    }
+}
